1d3a005ecb19fa728242be22d4c8fb3ef22eec2d
Emanuelle Oliveira <emanuelletrindadeoliveira@gmail.com>
2023-07-05 16:03:59 -0300

    fix: create item

--- app/src/main/java/com/example/fabricasapatos/domain/item/usecases/CreateItemUseCase.kt
+++ app/src/main/java/com/example/fabricasapatos/domain/item/usecases/CreateItemUseCase.kt
@@ -1,18 +1,22 @@
 package com.example.fabricasapatos.domain.item.usecases
 
 import com.example.fabricasapatos.data.item.ItemRepository
+import com.example.fabricasapatos.data.order.OrderRepository
 import com.example.fabricasapatos.domain.item.model.Item
 import com.example.fabricasapatos.domain.item.usecases.contracts.ICreateItemUseCase
 import javax.inject.Inject
 
 class CreateItemUseCase @Inject constructor(
-  private val itemRepository: ItemRepository
+  private val itemRepository: ItemRepository,
+  private val orderRepository: OrderRepository
 ): ICreateItemUseCase {
-  override suspend fun invoke(orderId: Int, productId: Int, quantity: Int): Item {
+  override suspend fun invoke(productId: Int, quantity: Int): Item {
 
     return try {
       val id = itemRepository.getLastItemId()
       val newId = itemRepository.updateLastItemId(id)
+
+      val orderId = orderRepository.getLastOrderId()
 
       val item = Item(newId, orderId, productId, quantity)
       itemRepository.createItem(item)
--- app/src/main/java/com/example/fabricasapatos/domain/item/usecases/contracts/ICreateItemUseCase.kt
+++ app/src/main/java/com/example/fabricasapatos/domain/item/usecases/contracts/ICreateItemUseCase.kt
@@ -3,5 +3,5 @@ package com.example.fabricasapatos.domain.item.usecases.contracts
 import com.example.fabricasapatos.domain.item.model.Item
 
 interface ICreateItemUseCase {
-  suspend operator fun invoke(orderId: Int, productId: Int, quantity: Int): Item
+  suspend operator fun invoke(productId: Int, quantity: Int): Item
 }
